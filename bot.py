d0b325e043b41da5d1a885a44437153a096f5cbe
Pranav2335 <106325687+Pranav2335@users.noreply.github.com>
2022-12-24 15:23:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging, os, math
 import logging.config
-from pyrogram import Client, __version__, filters
+from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
