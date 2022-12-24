22c80cd841e6249b832f4483568471a5240844a7
Pranav2335 <106325687+Pranav2335@users.noreply.github.com>
2022-12-24 12:06:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA")
+        m=await message.reply_sticker("CAACAgUAAxkBAAIFm2OhmqBI-GMtihYrl2PdLB0IcWA1AAJ5CgACtsSIV7CS8H_s1XvAHgQ")
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
